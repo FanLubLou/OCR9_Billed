ef75d4749af420ca7242ca30353379766817fc55
FanLubLou <jaclermont@hotmail.com>
2024-03-20 17:43:32 +0100

    scenario7-verifie si la modale apparait

--- Front/src/__tests__/Bills.js
+++ Front/src/__tests__/Bills.js
@@ -44,36 +44,89 @@ describe("Given I am connected as an employee", () => {
       expect(dates).toEqual(datesSorted)
     })
       
-     // Vérifie que le titre et le bouton sont bien affichés
+    // Scenario 5: Vérifie que le titre et le bouton sont bien affichés
     test('Then title and button should be displayed', () => {
-        document.body.innerHTML = BillsUI({ data: [] })
-        expect(screen.getAllByText("Mes notes de frais")).toBeTruthy()
-        expect(screen.getByTestId("btn-new-bill")).toBeTruthy()
-    }); 
-    // Vérifie que le formulaire de création de note de frais s'affiche bien
-    describe('When I click on "Nouvelle note de frais"', () => {
+      document.body.innerHTML = BillsUI({ data: [] })
+      expect(screen.getAllByText("Mes notes de frais")).toBeTruthy()
+      expect(screen.getByTestId("btn-new-bill")).toBeTruthy()
+  });
+
+  // Scenario 6: Vérifie que le formulaire de création de note de frais s'affiche bien
+  describe('When I click on "Nouvelle note de frais"', () => {
       test('Then the form to create a new invoice should appear', () => {
+          //onNavigate gère la navigation dans l'application lorsqu'un utilisateur clique sur un lien ou un bouton
           const onNavigate = (pathname) => {
               document.body.innerHTML = ROUTES({ pathname })
           }
+          // on remplace la méthode localStorage de l'objet window par une version mockée (localStorageMock). 
+          // Cela permet de simuler le comportement de localStorage dans un environnement de test.
           Object.defineProperty(window, 'localStorage', { value: localStorageMock })
           window.localStorage.setItem('user', JSON.stringify({
               type: 'Employee'
           }))
+          //Crée une instance de la classe Bills.
           const bills = new Bills({
               document,
               onNavigate,
               store: mockStore,
               localStorage: window.localStorage
           })
+          //On inject le HTML via la méthode BillsUI
           document.body.innerHTML = BillsUI({ data: bills });
-
+          //On récupère le bouton "Nouvelle note de frais" sur lequel on met un écouteurd evts
           const buttonNewBill = screen.getByTestId('btn-new-bill');
+          
+          //Crée un faux appel de fonction pour la méthode handleClickNewBill de l'objet bills
           const handleClickNewBill = jest.fn(bills.handleClickNewBill);
           buttonNewBill.addEventListener('click', handleClickNewBill);
+          //On déclenche un evt de clic
           fireEvent.click(buttonNewBill);
+          //On verifie que la fonction est bien appelée.
           expect(handleClickNewBill).toHaveBeenCalled();
       });
+    //Scenario 7
+    // Vérifie si la modale contenant le justificatif de la note de frais apparaît
+    describe('When I click on the icon eye', () => {
+      test('Then a modal should appear', () => {
+        // Le début est le même que le scenario6. 
+        // On simule le comportement du localStorage. On créée une instance de la classe Bills et on injecte grâce à BillsUI
+        // 
+          const onNavigate = (pathname) => {
+              document.body.innerHTML = ROUTES({ pathname })
+          };
+          Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+          window.localStorage.setItem('user', JSON.stringify({
+              type: 'Employee'
+          }));
+          const billsPage = new Bills({
+              document,
+              onNavigate,
+              store: mockStore,
+              localStorage: window.localStorage
+          });
+          document.body.innerHTML = BillsUI({ data: bills });
+        // On va ici sélectionner tous les elts icon-eye, surlesquels on va simuler le clic sur l'icône de l'oeil.
+          const iconEye = screen.getAllByTestId("icon-eye");
+          const handleClickIconEye = jest.fn(billsPage.handleClickIconEye);
+        // On va selectionner la modale sur laquelle on va ajouter la classe "show".
+          const modaleFile = document.getElementById("modaleFile");
+          $.fn.modal = jest.fn(() => modaleFile.classList.add("show"));
+        //Ici, pour chaque icone, on ajoute un gestionnaire d evts. On déclenche la fonction handlClickIconEye et on verifie si elle est appelée.
+          iconEye.forEach((icon) => {
+              icon.addEventListener("click", handleClickIconEye(icon))
+              fireEvent.click(icon)
+              expect(handleClickIconEye).toHaveBeenCalled()
+          });
+        //On vérifie si la classe show a ete ajoutee, si 'justificatif' est présent dans l'interface 
+        // et si l'URL du fichier associé à la première facture existe
+          expect(modaleFile).toHaveClass("show");
+          expect(screen.getByText("Justificatif")).toBeTruthy();
+          expect(bills[0].fileUrl).toBeTruthy();
+      });
+  });
+    
+    
+    
   });
 
   })
